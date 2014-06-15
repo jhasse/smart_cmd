2e70ab5ee8d4eb9e9ae28af7a11763e3ca6f2288
Jan Niklas Hasse <jhasse@gmail.com>
2014-06-15 21:53:18 +0200

    Work around HOME and USERPROFILE not being the same directory

--- src/msys.cpp
+++ src/msys.cpp
@@ -5,6 +5,9 @@
 
 int main() {
     auto tmp = getExplorerPath();
+    if (tmp.size() > 1 && tmp[1] != ':') {
+        tmp = std::string(getenv("USERPROFILE")) + "/" + tmp;
+    }
     std::replace(std::begin(tmp), std::end(tmp), '\\', '/');
 	std::cout << tmp;
 }
